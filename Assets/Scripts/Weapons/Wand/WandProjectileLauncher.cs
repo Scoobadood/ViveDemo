d345eb03f62184329cad9e08fc4487bd25bf9188
Dave Durbin <dave@moroku.com>
2017-10-16 18:44:49 +1100

    FEATURE: Added Projectil for wand and script to produce it.
    Imported the Pooling scripts to support projectile pooling

--- Assets/Scripts/Weapons/Wand/WandProjectileLauncher.cs
+++ Assets/Scripts/Weapons/Wand/WandProjectileLauncher.cs
@@ -2,10 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent( Wand)]
+[RequireComponent(typeof(Wand))]
 public class WandProjectileLauncher : MonoBehaviour {
+    // The projectil this thing fires
+    [SerializeField]
+    private Projectile projectilePrefab;
+
+
+    private Wand wand;
+
+
 
     private void Awake() {
         wand = GetComponent<Wand>();
+        wand.OnFire += Wand_OnFire;
+    }
+
+    /// <summary>
+    /// Spawn a new projectile from the pool on fire.
+    /// </summary>
+    void Wand_OnFire() {
+        projectilePrefab.Get<Projectile>( null, transform.position, transform.rotation );
     }
 }
